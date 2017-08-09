e8ac3979d779742686243b8763326456a9080ac6
prasanthkumar512 <jaligapu.prashanth512@gmail.com>
2017-08-09 12:43:12 +0530

    All Changes

---
+++ SmartTicketPortal/Controllers/HourBasedPricingController.cs
@@ -0,0 +1,103 @@
+﻿using SmartTicketPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartTicketDashboard.Controllers
+{
+    public class HourBasedPricingController : ApiController
+    {
+        
+        [HttpGet]
+        [Route("api/HourBasedPricing/GetHourBasePricing")]
+        public DataTable GetHourBasePricing()
+        {
+            DataTable Tbl = new DataTable();
+
+
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "PSGetHourBasePricing";
+            cmd.Connection = conn;
+
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+            db.Fill(Tbl);
+
+
+
+            // int found = 0;
+            return Tbl;
+
+        }
+
+        [HttpPost]
+        [Route("api/HourBasedPricing/SaveHourBasePricing")]
+        public DataTable SaveHourBasePricing(HourBase c)
+        {
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "PSInsUpdDelHourBasePricing";
+
+            cmd.Connection = conn;
+
+            SqlParameter insupddelflag = new SqlParameter("@insupddelflag", SqlDbType.VarChar, 10);
+            insupddelflag.Value = c.insupddelflag;
+            cmd.Parameters.Add(insupddelflag);
+
+            SqlParameter q = new SqlParameter("@Id", SqlDbType.Int);
+            q.Value = c.Id;
+            cmd.Parameters.Add(q);
+
+            SqlParameter v = new SqlParameter("@VehicleModelId", SqlDbType.Int);
+            v.Value = c.VehicleModelId;
+            cmd.Parameters.Add(v);
+
+            //SqlParameter q1 = new SqlParameter("@VehicleModel", SqlDbType.VarChar, 50);
+            //q1.Value = c.VehicleModel;
+            //cmd.Parameters.Add(q1);
+
+           SqlParameter e = new SqlParameter("@Hours", SqlDbType.VarChar, 5);
+            e.Value = c.Hours;
+            cmd.Parameters.Add(e);
+
+            SqlParameter p = new SqlParameter("@Price", SqlDbType.Decimal);
+            p.Value = c.Price;
+            cmd.Parameters.Add(p);
+
+            SqlParameter ft = new SqlParameter("@FromTime", SqlDbType.DateTime);
+            ft.Value = c.FromTime;
+            cmd.Parameters.Add(ft);
+
+            SqlParameter tf = new SqlParameter("@ToTime", SqlDbType.DateTime);
+            tf.Value = c.ToTime;
+            cmd.Parameters.Add(tf);
+         
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            return (dt);
+
+
+
+        }
+
+    }
+}
+
+  
---
+++ SmartTicketPortal/UI/js/HireVehicle.js
@@ -0,0 +1,117 @@
+﻿
+var app = angular.module('myApp', ['ngStorage', 'ui.bootstrap'])
+
+var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
+    $scope.GetStops = function () {
+
+        $http.get('/api/Stops/GetStops').then(function (response, req) {
+            $scope.Stops = response.data;
+            $localStorage.Stops = $scope.Stops;
+        }, function (data) {
+
+        });
+
+        $http.get('/api/Stops/TypesByGroupId?groupid=3').then(function (res, data) {
+            $scope.licenses = res.data;
+
+        });
+    }
+ 
+    $scope.GetHireVehicle = function () {
+        if ($scope.S == null) {
+            // alert('Please select source.');
+
+            $scope.showDialog('Please select source.');
+            return;
+        }
+
+        if ($scope.D == null) {
+            $scope.showDialog('Please select destination.');
+            return;
+        }
+
+        $localStorage.src = $scope.S;
+        $localStorage.dest = $scope.D;
+
+        //$rootscope.src = $scope.RS;
+        //$rootscope.dest = $scope.RD;
+        $localStorage.timing = ($scope.timing == 'Now') ? Date() : $scope.timing;
+
+        $localStorage.triptype = $scope.triptype;
+        $scope.PSGetHourbasedpricing();
+
+        //$scope.booking = [
+        //    { "Id": 1, "Company": "CMP1", "VehicleType": "Indigo", "StartTime": "11:00 AM", "EndTime": "12:30 PM", "Rating": "3", "Price": "10", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 2, "Company": "CMP2", "VehicleType": "BMW",  "StartTime": "11:10 AM", "EndTime": "12:30 PM", "Rating": "4", "Price": "12", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 3, "Company": "CMP3", "VehicleType": "AC",  "StartTime": "12:00 PM", "EndTime": "12:30 PM", "Rating": "3", "Price": "10", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 4, "Company": "CMP4", "VehicleType": "Non-AC", "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "13", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 5, "Company": "CMP5", "VehicleType": "AC",  "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "11", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 6, "Company": "CMP6", "VehicleType": "AC",  "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "11", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 7, "Company": "CMP7", "VehicleType": "AC",  "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "12", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 8, "Company": "CMP8", "VehicleType": "AC", "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "11", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 9, "Company": "CMP9", "VehicleType": "Non-AC", "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "10", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 10, "Company": "CMP10", "VehicleType": "Non-AC", "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "11", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 11, "Company": "CMP11", "VehicleType": "AC",  "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "13", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 12, "Company": "CMP12", "VehicleType": "AC",  "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "12", "luggageCrg": "", "Discount": "" }
+        //    , { "Id": 13, "Company": "CMP13", "VehicleType": "AC", "StartTime": "12:30 PM", "EndTime": "12:30 PM", "Rating": "", "Price": "11", "luggageCrg": "", "Discount": "" }
+        //];
+
+    }
+    $scope.FillInitialDetails = function (indx, vDetails) {
+
+        if ($scope.selectedIndex != -1)
+            document.getElementById('t_' + $scope.selectedIndex).style.display = "none";
+
+        var currstyle = document.getElementById('t_' + indx).style.display;
+        // var currstyle = document.getElementById('imgTd').style.display;
+
+        if (currstyle == "none") {
+            document.getElementById('t_' + indx).style.display = "table-cell";
+            $scope.selectedIndex = indx;
+            // $scope.selectedSeats = [];
+        }
+        //   $scope.basePrice = b.amount;
+        $scope.onwarddetails = new Object();
+        $scope.onwarddetails.VehicleId = vDetails.Id;
+       // $scope.onwarddetails.TicketNo = $scope.GetTicketNo();
+        $scope.onwarddetails.JourneyDate = new Date();
+        $scope.onwarddetails.JourneyTime = new Date();
+
+        $scope.onwarddetails.Src = $localStorage.src.Name;
+        $scope.onwarddetails.Dest = $localStorage.dest.Name;
+        $scope.onwarddetails.SrcId = $localStorage.src.Id;
+        $scope.onwarddetails.DestId = $localStorage.dest.Id;
+
+        $scope.onwarddetails.EmailId = '';
+        $scope.onwarddetails.MobileNo = '';
+        $scope.onwarddetails.AltMobileNo = '';
+        $scope.onwarddetails.Address = '';
+        $scope.onwarddetails.perunitprice = '';
+        $scope.onwarddetails.JourneyType = 'Onward';
+        $scope.onwarddetails.bookingType = 'Online';
+        $scope.onwarddetails.bookedBy = 'Onward';
+        $scope.onwarddetails.userid = 'Onward';
+        $scope.onwarddetails.NoOfSeats = 0;
+        $scope.onwarddetails.seatslist = '';
+        $scope.onwarddetails.amount = 0;//
+        $scope.onwarddetails.insupddelflag = 'I';//
+        $scope.onwarddetails.BookedSeats = [];
+    }
+    $scope.GetHourBasePricing = function () {
+      //  $scope.vm = $localStorage.vm;
+      //  $scope.Price = $localStorage.Price;
+      // // $scope.timing = $localStorage.timing;
+
+
+      //  $scope.VehicleModelId = $localStorage.vm.Id;
+      ////  $scope.destId = $localStorage.dest.Id;
+      //  $scope.srcStage = $localStorage.src.name;
+      //  $scope.destStage = $localStorage.dest.name;
+      //  $scope.triptype = $localStorage.triptype;
+
+
+        $http.get('/api/HourBasedPricing/GetHourBasePricing').then(function (response, req) {
+            $scope.booking = response.data;
+        });
+    }
+});
