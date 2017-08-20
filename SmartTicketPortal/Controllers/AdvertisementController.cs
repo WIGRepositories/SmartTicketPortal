2be8a1d864667d366be73ac5db824fb62b6220bf
gundapurna <venkatapurnachandrarao71@gmail.com>
2017-08-20 11:24:58 +0530

    images changes

---
+++ SmartTicketPortal/Controllers/AdvertisementController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartTicketPortal.Controllers
+{
+    public class AdvertisementController : ApiController
+    {
+        [HttpGet]
+        public DataTable GetAdvertisment()
+        {
+            DataTable Tbl = new DataTable();
+         
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "Getadvertisement";
+            cmd.Connection = conn;
+            DataSet ds = new DataSet();
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+            db.Fill(ds);
+            Tbl = ds.Tables[0];
+           
+            return Tbl;
+
+        }
+    }
+}
