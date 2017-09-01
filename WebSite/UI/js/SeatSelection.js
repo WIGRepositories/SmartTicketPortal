4db91792d35355499347529dbf38873940cf54a0
gundapurna <venkatapurnachandrarao71@gmail.com>
2017-09-01 11:24:12 +0530

    loging changes

---
+++ WebSite/UI/js/SeatSelection.js
@@ -0,0 +1,76 @@
+﻿// JavaScript source code
+var myapp1 = angular.module('myApp', [])
+var mycrtl1 = myapp1.controller('Mycntrlr', function ($scope, $http) {
+
+    $http.get('http://localhost:1476/api/KESENINE3/commericialsite').then(function (res, data) {
+        $scope.tr = res.data;
+
+
+    })
+
+
+    {    //This will hide the DIV by default.
+        $scope.IsHidden = true;
+        $scope.ShowHide = function () {
+            //If DIV is hidden it will be visible and vice versa.
+            $scope.IsHidden = $scope.IsHidden ? false : true;
+        }
+    }
+});
+//$scope.album = [{
+//    id: 1,
+//    name: 11
+//},
+//  {
+//      id: 2,
+//      name: 12
+//  },
+//  {
+//      id: 3,
+//      name: 13,
+//  },
+//  {
+//      id: 4,
+//      name: 14
+//  },
+
+//   {
+//       id: 5,
+//       name: 15,
+//   },
+
+//    {
+//        id: 6,
+//        name: 16,
+//    },
+
+
+//    {
+//        id: 7,
+//        name: 17,
+//    },
+
+//    {
+//        id: 8,
+//        name: 18,
+//    },
+//    {
+//        id: 9,
+//        name: 19
+
+//  }];
+
+//$scope.save = function (album) {
+//    var album = {
+
+//        id: album.id,
+//        name: album.name
+
+//    };
+//    $scope.albumNameArray = [];
+//    angular.forEach($scope.album, function (album) {
+//        if (album.selected) $scope.albumNameArray.push(album.name);
+//    });
+
+//}
+//});
---
+++ WebSite/UI/js/TicketCheckout.js
@@ -0,0 +1,71 @@
+﻿var app = angular.module('myApp', ['ngStorage'])
+var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $rootScope) {
+
+    $rootScope.spinner = {
+        active: false,
+        on: function () {
+            this.active = true;
+        },
+        off: function () {
+            this.active = false;
+        }
+    }
+
+    $scope.book1 = $scope.$localStorage;
+
+    $scope.onwarddetails = $localStorage.onwarddetails;
+    $scope.BookingId = $localStorage.BookingId;
+
+    //retrive the emailid and mobile no and try to get the user details from db
+    //if not exists then show empty
+    $scope.getcheckoutdetails = function () {
+
+        var emailid = $scope.onwarddetails.EmailId;
+        $http.get('/api/websiteuserinfo/GetWebsiteUserInfo?logininfo=' + emailid).then(function (response, data) {
+            $scope.userdetails = response.data[0];
+        });
+    }
+
+
+    $scope.ProceedToPayment = function () {
+
+        $rootScope.spinner.on();
+
+        $scope.onwarddetails = $localStorage.onwarddetails;
+
+        if ($scope.onwarddetails == null) {
+            alert('Error occurred during ticket booking. Please retry or contact INTERBUS administrator if the problem persists.');
+            return;
+        }
+
+        $http({
+            url: '/api/TicketBooking/SaveBookingDetails',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: $scope.onwarddetails
+        }).success(function (data, status, headers, config) {
+            if (data == null) {
+                $rootScope.spinner.off();
+                alert('Error during ticket booking. Please re-try.')
+                return;
+            }
+            $scope.BookingId = data;
+
+            if ($scope.BookingId == null || $scope.BookingId == -1) {
+                $rootScope.spinner.off();
+                alert('Error during ticket booking. Please re-try.')
+                return;
+            }
+            $localStorage.BookingId = $scope.BookingId;
+
+            window.location.href = "TicketPage.html";
+
+        }).error(function (ata, status, headers, config) {
+            $rootScope.spinner.off();
+            alert('Error during ticket booking. Please re-try. Details:' + ata);
+        });
+
+        //alert('Payment gateway integration will done here and on successful payment user will be redirect to ticket printing page.');      
+    }
+
+});
