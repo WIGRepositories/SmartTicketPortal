bffea6fa6a2a844716b51674bc7ad33b577ec032
gundapurna <venkatapurnachandrarao71@gmail.com>
2017-09-06 18:49:19 +0530

    changes

---
+++ SmartTicketPortal/Controllers/CarouselController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Tracing;
+
+namespace SmartTicketPortal.Controllers
+{
+    public class CarouselController : ApiController
+    {
+        [HttpGet]
+        public DataTable GetCarousel()
+        {
+            DataTable Tbl = new DataTable();
+            LogTraceWriter traceWriter = new LogTraceWriter();
+            traceWriter.Trace(Request, "0", TraceLevel.Info, "{0}", "GetCarousel credentials....");
+
+
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "GetCarouselImages";
+            cmd.Connection = conn;
+            DataSet ds = new DataSet();
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+            db.Fill(ds);
+            Tbl = ds.Tables[0];
+            traceWriter.Trace(Request, "0", TraceLevel.Info, "{0}", "GetCarousel Credentials completed.");
+            // int found = 0;
+            return Tbl;
+
+        }
+    }
+}
