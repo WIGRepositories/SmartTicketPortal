a7840c0da9ffe953fa8606809bfc96abb82a1771
gundapurna <venkatapurnachandrarao71@gmail.com>
2018-01-17 11:36:17 +0530

    ui changes

--- SmartTicketPortal/UI/js/Get-a-lyft.js
+++ SmartTicketPortal/UI/js/Get-a-lyft.js
@@ -1,37 +1,39 @@
-﻿var app = angular.module('plunker', ['google-maps']);
+﻿var app = angular.module('myApp', ['google-maps', 'vsGoogleAutocomplete']);
+app.controller('mapCtrl', function ($scope, $http) {
 
-app.controller('MainCtrl', function ($scope, $document,$http) {
-    // map object
-    $scope.map = {
-        control: {},
-        center: {
-            latitude: 17.3850,
-            longitude: 78.4867
-        },
-        zoom: 14
-    };
+    //$scope.map = {
+    //    control: {},
+    //    center: {
+    //        latitude: 17.3850,
+    //        longitude: 78.4867
+    //    },
+    //    zoom: 20
+    //};
 
-    // marker object
-    $scope.marker = {
-        center: {
-            latitude: 17.3850,
-            longitude: 78.4867
-        }
+    //$scope.map = {
+    //    control: {},
+    //    center: {
+    //        latitude: 17.3850,
+    //        longitude: 78.4867
+    //    },
+    //    zoom: 16
+    //};
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(17.3850, 78.4867),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
-    // instantiate google map objects for directions
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $scope.markers = [];
+    $scope.cities = [];
+
     var directionsDisplay = new google.maps.DirectionsRenderer();
     var directionsService = new google.maps.DirectionsService();
     var geocoder = new google.maps.Geocoder();
 
-    // directions object -- with defaults
-    $scope.directions = {
-        origin: "Lampex Electronics Limited, IDA Kukatpally, Hyderabad, Telangana",
-        destination: "webingate solutions pvt ltd., Erragadda, Hyderabad, Telangana",
-        showList: false
-    }
 
-    // get directions using google maps api
     $scope.getDirections = function () {
         var request = {
             origin: $scope.directions.origin,
@@ -39,98 +41,111 @@ app.controller('MainCtrl', function ($scope, $document,$http) {
             travelMode: google.maps.DirectionsTravelMode.DRIVING
         };
         directionsService.route(request, function (response, status) {
-            if (status === google.maps.DirectionsStatus.OK) {
+            if (status == google.maps.DirectionsStatus.OK) {
                 directionsDisplay.setDirections(response);
-                directionsDisplay.setMap($scope.map.control.getGMap());
+                directionsDisplay.setMap($scope.map);
+                // directionsDisplay.setPanel(document.getElementById('distance').innerHTML += response.routes[0].legs[0].distance.value + " meters");
+                $scope.distText = response.routes[0].legs[0].distance.text;
+                $scope.distval = response.routes[0].legs[0].distance.value;
+                //response.routes[0].bounds["f"].b
+                //17.43665
+                //response.routes[0].bounds["b"].b
+                //78.41263000000001
+
+
+                //response.routes[0].bounds["f"].f
+                //17.45654
+                //response.routes[0].bounds["b"].f
+                //78.44829                
+
+                $scope.srcLat = response.routes[0].bounds["f"].b;
+                $scope.srcLon = response.routes[0].bounds["b"].b;
+                $scope.destLat = response.routes[0].bounds["f"].f;
+                $scope.destLon = response.routes[0].bounds["b"].f;
+                $scope.directions.showList = true;
+            } else {
+                alert('Google route unsuccesfull!');
+            }
+
+        });
+    }
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    $scope.getDirections = function () {
+        //get the source latitude and longitude
+        //get the target latitude and longitude
+        $scope.srcLat = $scope.pickupPoint.place.geometry.location.lat();
+        $scope.srcLon = $scope.pickupPoint.place.geometry.location.lng();
+        $scope.destLat = $scope.dropPoint.place.geometry.location.lat();
+        $scope.destLon = $scope.dropPoint.place.geometry.location.lng();
+
+        $scope.srcName = $scope.pickupPoint.place.name;
+        $scope.destName = $scope.dropPoint.place.name;
+        //alert($scope.dropPoint.place.geometry.location.lat);
+        var request = {
+            origin: new google.maps.LatLng($scope.srcLat, $scope.srcLon),//$scope.directions.origin,
+            destination: new google.maps.LatLng($scope.destLat, $scope.destLon),//$scope.directions.destination,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                directionsDisplay.setMap($scope.map);
+                // directionsDisplay.setPanel(document.getElementById('distance').innerHTML += response.routes[0].legs[0].distance.value + " meters");
 
                 $scope.distval = response.routes[0].legs[0].distance.value / 1000;
                 $scope.distText = $scope.distval + " KM";
-                //directionsDisplay.setPanel(document.getElementById('directionsList'));
+
+                //response.routes[0].bounds["f"].b
+                //17.43665
+                //response.routes[0].bounds["b"].b
+                //78.41263000000001
+
+
+                //response.routes[0].bounds["f"].f
+                //17.45654
+                //response.routes[0].bounds["b"].f
+                //78.44829                
+
+                //$scope.srcLat = response.routes[0].bounds["f"].b;
+                //$scope.srcLon = response.routes[0].bounds["b"].b;
+                //$scope.destLat = response.routes[0].bounds["f"].f;
+                //$scope.destLon = response.routes[0].bounds["b"].f;              
+
                 //$scope.directions.showList = true;
             } else {
                 alert('Google route unsuccesfull!');
             }
-            $scope.SetTotal();
-        });
-        $scope.step = 2;
-    }
-    
 
-    $scope.step = 1;
-    $scope.test = function () {
-        $scope.step = 3
-    };
-    $scope.SetTotal = function () {
-
-        distance: $scope.distText
-
-        var dist = {
-            distance: $scope.distText,
-            packageId:1
-        }
-
-        //$scope.total = eval($scope.unitprice) * eval($scope.distval);
-        var req = {
-            method: 'POST',
-            url: '/api/BookAVehicle/CalculatePrice',
-            data: dist
-        }
-        $http(req).then(function (response) {
-            var res = response.data;
         });
     }
 
-    //var inputcred = { distance: $scope.distText }
+    //$http.get('http://localhost:1476/api/Tracking/GetLatLongHistory').
+    //    success(function (data) {
 
-    $scope.SaveNew = function (book) {
-        //alert();
+    //        $scope.cities = data;
+    //        $scope.cities.forEach(function (city) {
+    //            createMarker(city);
+    //        });
 
+    //    });
 
-        var vdpc = {
-            Id: -1,
-            Src: $scope.directions.origin,
-            Dest: $scope.directions.destination,
-            SrcLatitude: $scope.srcLat,
-            SrcLongitude: $scope.srcLon,
-            DestLatitude: $scope.destLat,
-            DestLongitude: $scope.destLon,
-            packageId: 1,
-            CustomerPhoneNo: book.CustomerPhoneNo,
-            BookingStatus: $scope.BookingStatus,
-            BookingChannel: $scope.BookingChannel,
-            //PricingTypeId: directions.pricing,
-            Pricing: $scope.pricing,
-            distance: $scope.distText,
-            UnitPrice: $scope.unitprice,
-            Amount: $scope.Price,
-            flag: 'I'
-        }
+    //var createMarker = function (city) {
+    //    var marker = new google.maps.Marker({
+    //        map: $scope.map,
+    //        position: new google.maps.LatLng(city.Latitude, city.Longitude),
+    //        title: city.city
 
-        var req = {
-            method: 'POST',
-            url: '/api/BookAVehicle/SaveBookingDetails',
-            data: vdpc
-        }
+    //    });
+    //    marker.content = '<div class="infoWindowContent">' + city.description + '</div>';
 
-        $http(req).then(function (response) {
+    //    google.maps.event.addListener(marker, 'click', function () {
+    //        infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+    //        infoWindow.open($scope.map, marker);
+    //    });
 
-            //alert("Booking successful!");
-            $scope.Group = null;
-
-        }, function (errres) {
-            var errdata = errres.data;
-            var errmssg = "";
-            errmssg = (errdata && errdata.ExceptionMessage) ? errdata.ExceptionMessage : errdata.Message;
-            $scope.showDialog(errmssg);
-        });
-    }
-   
-    
-    //$http(req).then(function (res) {
-
-    //    if (res.data.length == 0) {
-    //        alert('invalid credentials');
-    //    }
-    //});
+    //    $scope.markers.push(marker);
+    //};
 
 });
