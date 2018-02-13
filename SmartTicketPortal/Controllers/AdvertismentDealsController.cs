18e0ff3204c45c2f7b39bfbf5499856eee5d72a8
saikumar7084 <boddu.saikumar1994@gmail.com>
2018-02-13 17:22:21 +0530

    ui

---
+++ SmartTicketPortal/Controllers/AdvertismentDealsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartTicketPortal.Controllers
+{
+    public class AdvertismentDealsController : ApiController
+    {
+
+        [HttpGet]
+        [Route("api/AdvertismentDeals/GetAdvertismentDeal")]
+        public DataTable GetAdvertismentDeal()
+        {
+            DataTable Tbl = new DataTable();
+            //LogTraceWriter traceWriter = new LogTraceWriter();
+            //traceWriter.Trace(Request, "0", TraceLevel.Info, "{0}", "GetAdvertismentDeals credentials....");
+
+
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "GetAdvertismentDeals";
+            cmd.Connection = conn;
+            DataSet ds = new DataSet();
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+            db.Fill(ds);
+            Tbl = ds.Tables[0];
+            //traceWriter.Trace(Request, "0", TraceLevel.Info, "{0}", "GetAdvertismentDeals credentials....");
+            // int found = 0;
+            return Tbl;
+        }
+    }
+}
